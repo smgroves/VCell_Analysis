a78d7dbb3e46138dd5a442d3e3a355657ea9bf24
Sarah Groves <sarahmaddoxgroves@gmail.com>
2023-08-25 12:44:39 -0400

    Update conversions.sh

--- conversions.sh
+++ conversions.sh
@@ -6,3 +6,11 @@ python hdf5_converter.py "SimID_259737823_0__exported.hdf5" '/Users/smgroves/Box
 
 # 8/23/23
 python hdf5_converter.py "SimID_259759333_0__exported.hdf5" '/Users/smgroves/Box/CPC_Model_Project/VCell_Exports/From Catalina' "08_21_23_CPC_relaxed_RefModel_Mps1_phos_Plk1a transactiv" "08_21_23_relaxed_RefModel_Mps1_phos_Plk1a transactiv_KmMps1_5.4"
+python hdf5_converter.py "SimID_259772019_0__exported.hdf5" '/Users/smgroves/Box/CPC_Model_Project/VCell_Exports/From Catalina' "08_21_23_CPC_relaxed_RefModel_Mps1_phos_Plk1a transactiv" "08_28_23_relaxed_RefModel_Mps1_phos_Plk1a transactiv_KmMps1_5.4 HaspInh"
+
+# 8/25/23
+python hdf5_converter.py "SimID_259825206_0__exported.hdf5" '/Users/smgroves/Box/CPC_Model_Project/VCell_Exports/From Catalina' "08_21_23_CPC_relaxed_RefModel_Mps1_phos_Plk1a transactiv" "08_28_23_relaxed_RefModel_Mps1_phos_Plk1a transactiv kppKT=0.1"
+python hdf5_converter.py "SimID_259825209_0__exported.hdf5" '/Users/smgroves/Box/CPC_Model_Project/VCell_Exports/From Catalina' "08_21_23_CPC_relaxed_RefModel_Mps1_phos_Plk1a transactiv" "08_28_23_relaxed_RefModel_Mps1_phos_Plk1a transactiv kppKT=0.1 Haspinh"
+python hdf5_converter.py "SimID_259825234_0__exported.hdf5" '/Users/smgroves/Box/CPC_Model_Project/VCell_Exports/From Catalina' "08_21_23_CPC_relaxed_RefModel_Mps1_phos_Plk1a transactiv" "08_28_23_relaxed_RefModel_Mps1_phos transactiv kppKT=0.2"
+python hdf5_converter.py "SimID_259825237_0__exported.hdf5" '/Users/smgroves/Box/CPC_Model_Project/VCell_Exports/From Catalina' "08_21_23_CPC_relaxed_RefModel_Mps1_phos_Plk1a transactiv" "08_28_23_relaxed_RefModel_Mps1_phos transactiv kppKT=0.2 Haspinh"
+
