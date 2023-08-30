ebe595a77edea32612d99618b4180b7a39406017
Sarah Groves <sarahmaddoxgroves@gmail.com>
2023-08-30 10:46:57 -0400

    additional conversions + code for manipulating IC for relaxed-to-tense

--- conversions.sh
+++ conversions.sh
@@ -14,3 +14,23 @@ python hdf5_converter.py "SimID_259825209_0__exported.hdf5" '/Users/smgroves/Box
 python hdf5_converter.py "SimID_259825234_0__exported.hdf5" '/Users/smgroves/Box/CPC_Model_Project/VCell_Exports/From Catalina' "08_21_23_CPC_relaxed_RefModel_Mps1_phos_Plk1a transactiv" "08_28_23_relaxed_RefModel_Mps1_phos transactiv kppKT=0.2"
 python hdf5_converter.py "SimID_259825237_0__exported.hdf5" '/Users/smgroves/Box/CPC_Model_Project/VCell_Exports/From Catalina' "08_21_23_CPC_relaxed_RefModel_Mps1_phos_Plk1a transactiv" "08_28_23_relaxed_RefModel_Mps1_phos transactiv kppKT=0.2 Haspinh"
 
+#8/26/23
+python hdf5_converter.py "SimID_259801922_0__exported.hdf5" '/Users/smgroves/Box/CPC_Model_Project/VCell_Exports/' "SMG_08_21_23_CPC_relaxed_RefModel_Mps1_phos_Plk1a transactiv" "08_21_23_relaxed_RefModel_Mps1_phos_Plk1a_20Pac transactiv"
+
+python hdf5_converter.py "SimID_259881181_0__exported.hdf5" '/Users/smgroves/Box/CPC_Model_Project/VCell_Exports/From Catalina' "08_21_23_CPC_relaxed_RefModel_Mps1_phos_Plk1a transactiv" "08_28_23_relaxed_RefModel_Mps1_phos_Plk1a_20Pac transactiv kppKT=0.1 Inhib"
+python hdf5_converter.py "SimID_259881184_0__exported.hdf5" '/Users/smgroves/Box/CPC_Model_Project/VCell_Exports/From Catalina' "08_21_23_CPC_relaxed_RefModel_Mps1_phos_Plk1a transactiv" "08_28_23_relaxed_RefModel_Mps1_phos_Plk1a_20Pac transactiv kppKT=0.2  Inhib"
+python hdf5_converter.py "SimID_259833335_0__exported.hdf5" '/Users/smgroves/Box/CPC_Model_Project/VCell_Exports/From Catalina' "08_28_23_CPC_relaxed_RefModel_Mps1_phos_Plk1a transactiv" "08_21_23_tensed_RefModel_Mps1_phos_Plk1s transactiv"
+python hdf5_converter.py "SimID_259833347_0__exported.hdf5" '/Users/smgroves/Box/CPC_Model_Project/VCell_Exports/From Catalina' "08_28_23_CPC_relaxed_RefModel_Mps1_phos_Plk1a transactiv" "08_28_23_tensed_RefModel_Mps1_phos_Plk1a_20Pac transactiv kppKT=0.1"
+python hdf5_converter.py "SimID_259880936_0__exported.hdf5" '/Users/smgroves/Box/CPC_Model_Project/VCell_Exports/From Catalina' "08_28_23_CPC_relaxed_RefModel_Mps1_phos_Plk1a transactiv" "08_28_23_tensed_RefModel_Mps1_phos_Plk1a_20Pac transactiv kppKT=0.1 Inhib"
+python hdf5_converter.py "SimID_259833350_0__exported.hdf5" '/Users/smgroves/Box/CPC_Model_Project/VCell_Exports/From Catalina' "08_28_23_CPC_relaxed_RefModel_Mps1_phos_Plk1a transactiv" "08_28_23_tensed_RefModel_Mps1_phos_Plk1a_20Pac transactiv kppKT=0.2"
+python hdf5_converter.py "SimID_259880939_0__exported.hdf5" '/Users/smgroves/Box/CPC_Model_Project/VCell_Exports/From Catalina' "08_28_23_CPC_relaxed_RefModel_Mps1_phos_Plk1a transactiv" "08_28_23_tensed_RefModel_Mps1_phos_Plk1a_20Pac transactiv kppKT=0.2 Inhib"
+python hdf5_converter.py "SimID_259833354_0__exported.hdf5" '/Users/smgroves/Box/CPC_Model_Project/VCell_Exports/From Catalina' "08_28_23_CPC_relaxed_RefModel_Mps1_phos_Plk1a transactiv" "08_28_23_tensed_RefModel_Mps1_phos_Plk1a transactiv KmMps1_5.4"
+python hdf5_converter.py "SimID_259833356_0__exported.hdf5" '/Users/smgroves/Box/CPC_Model_Project/VCell_Exports/From Catalina' "08_28_23_CPC_relaxed_RefModel_Mps1_phos_Plk1a transactiv" "08_28_23_tensed_RefModel_Mps1_phos_Plk1a transactiv KmMps1_5.4 HaspInh"
+
+#8/30/23
+python hdf5_converter.py "SimID_259924058_0__exported.hdf5" '/Users/smgroves/Box/CPC_Model_Project/VCell_Exports/From Catalina' "08_21_23_CPC_relaxed_RefModel_Mps1_phos_Plk1a transactiv" "08_21_23_relaxed_RefModel_Mps1_phos_Plk1a transactiv Knl1_inh"
+python hdf5_converter.py "SimID_259924061_0__exported.hdf5" '/Users/smgroves/Box/CPC_Model_Project/VCell_Exports/From Catalina' "08_21_23_CPC_relaxed_RefModel_Mps1_phos_Plk1a transactiv" "08_28_23_relaxed_RefModel_Mps1_phos_Plk1a_20Pac transactiv kppKT=0.1 Knl1_Inh"
+python hdf5_converter.py "SimID_259924064_0__exported.hdf5" '/Users/smgroves/Box/CPC_Model_Project/VCell_Exports/From Catalina' "08_21_23_CPC_relaxed_RefModel_Mps1_phos_Plk1a transactiv" "08_28_23_relaxed_RefModel_Mps1_phos_Plk1a transactiv_KmMps1_5.4 Knl1_Inh"
+python hdf5_converter.py "SimID_259924181_0__exported.hdf5" '/Users/smgroves/Box/CPC_Model_Project/VCell_Exports/From Catalina' "08_28_23_CPC_relaxed_RefModel_Mps1_phos_Plk1a transactiv" "08_21_23_relaxed_RefModel_Mps1_phos_Plk1a transactiv Knl1_inh"
+python hdf5_converter.py "SimID_259924184_0__exported.hdf5" '/Users/smgroves/Box/CPC_Model_Project/VCell_Exports/From Catalina' "08_28_23_CPC_relaxed_RefModel_Mps1_phos_Plk1a transactiv" "08_28_23_relaxed_RefModel_Mps1_phos_Plk1a_20Pac transactiv kppKT=0.1 Knl1_Inh"
+python hdf5_converter.py "SimID_259924187_0__exported.hdf5" '/Users/smgroves/Box/CPC_Model_Project/VCell_Exports/From Catalina' "08_28_23_CPC_relaxed_RefModel_Mps1_phos_Plk1a transactiv" "08_28_23_relaxed_RefModel_Mps1_phos_Plk1a transactiv_KmMps1_5.4 Knl1_Inh"
---
+++ relaxed_to_tense.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+#read in relaxed state simulation results from csv files at 100s
+
+#For Ndc80, move input out so that it is still at the KT
+
+
