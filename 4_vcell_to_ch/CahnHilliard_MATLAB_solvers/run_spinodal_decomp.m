bc808b6ac4c335666fce193ffd31eb47c449661a
Sarah Groves <sarahmaddoxgroves@gmail.com>
2025-11-06 21:18:58 -0500

    updates to vcell to ch and pinferna genes

--- 4_vcell_to_ch/CahnHilliard_MATLAB_solvers/run_spinodal_decomp.m
+++ 4_vcell_to_ch/CahnHilliard_MATLAB_solvers/run_spinodal_decomp.m
@@ -1,6 +1,6 @@
  % FIGURE 1
-indir = "/Users/catalinaalvarez/Documents/cpc_ch_data_2025";
-outdir = "/Users/catalinaalvarez/Documents/cpc_ch_data_2025/output_MATLAB";
+indir = "/Users/smgroves/Documents/GitHub/VCell_Analysis/4_vcell_to_ch/IC";
+outdir = "/Users/smgroves/Documents/GitHub/VCell_Analysis/4_vcell_to_ch/output";
 
 n_relax = 4;
 m = 8;
@@ -13,50 +13,17 @@ epsilon = 0.0089; %10A
 dt = 2.5e-5;
 max_it = 2000;
 boundary = 'neumann';
-init_file = sprintf("%s/09_30_25_metacentric_relaxed_model_HeLa_CPC_Sgo1low_pH2Ainh_1_2_5.xlsx",indir);
+init_file = sprintf("%s/_09_16_25_CPC_metacentric_relaxed_model_09_16_25_metacentric_relaxed_model_120_256x256_5max.csv",indir);
 phi0 = readmatrix(init_file);
 print_phi = true;
 dt_out = 1;
 ny = GridSize;
-% #################################################
-% RUN SAV SOLVER 
-% #################################################
-
-% pathname = sprintf("%s/tensed_state_mod_SAV_output",outdir);
-% fprintf("Running SAV solver with parameters: %s\n", pathname);
-% tStart_SAV = tic;
-% [t_out, phi_t, delta_mass_t, E_t] = CahnHilliard_SAV(phi0,...
-%                                     t_iter = max_it,...
-%                                     dt = dt,...
-%                                     m = m,...
-%                                     boundary = boundary,...
-%                                     printphi=print_phi,...
-%                                     pathname=pathname,...
-%                                     dt_out = dt_out);
-% elapsedTime = toc(tStart_SAV);
-% 
-% % fid = fopen('../Job_specs.csv', 'a+');
-% % v = [string(datetime) "SAV_spinodal_decomp_smoothed_periodic_dtout_10_relaxation" "MATLAB" "SAV" GridSize epsilon dt 'NaN' max_it 'NaN' pathname elapsedTime "NaN" boundary];
-% % fprintf(fid, '%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n', v);
-% % fclose(fid);
-% 
-% % writematrix(phi_t(:,:,end),sprintf('%sfinal_phi.csv', pathname));
-% writematrix(delta_mass_t,sprintf('%smass_uncentered.csv', pathname));
-% writematrix(E_t,sprintf('%senergy.csv', pathname));
-% 
-% fprintf("Creating movie\n");
-% filename = strcat(pathname, "movie");
-% if print_phi
-%     ch_movie_from_file(strcat(pathname,"phi.csv"), t_out, ny,filename = filename)
-% else
-%     ch_movie(phi_t,t_out, filename = filename);
-% end
 
 % % #################################################
 % % RUN NMG SOLVER 
 % % #################################################
 
-pathname = sprintf("%s/09_30_25_metacentric_relaxed_model_HeLa_CPC_Sgo1low_pH2Ainh_1_2_5_10Ae_10x_output", outdir);
+pathname = sprintf("%s/_09_16_25_CPC_metacentric_relaxed_model_09_16_25_metacentric_relaxed_model_120_256x256_5max", outdir);
 fprintf("Running NMG solver with parameters: %s\n", pathname);
 tStart_NMG = tic;
 [t_out, phi_t, delta_mass_t, E_t] = CahnHilliard_NMG(phi0,...
@@ -70,11 +37,6 @@ tStart_NMG = tic;
                                     tol = 1e-6);
 elapsedTime = toc(tStart_NMG);
 
-fid = fopen('../Job_specs.csv', 'a+');
-v = [string(datetime) "NMG_spinodal_decomp_smoothed_dtout_10" "MATLAB" "NMG" GridSize epsilon dt 'NaN' max_it  pathname elapsedTime 'NaN' "periodic"];
-fprintf(fid, '%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n', v);
-fclose(fid);
-
 % writematrix(phi_t(:,:,end),sprintf('%sfinal_phi.csv', pathname));
 writematrix(delta_mass_t,sprintf('%smass_uncentered.csv', pathname));
 writematrix(E_t,sprintf('%senergy.csv', pathname));
@@ -86,38 +48,3 @@ else
     ch_movie(phi_t,t_out, filename = filename);
 end
 
-
-
-% % #################################################
-% % RUN FD SOLVER 
-% % #################################################
-
-% pathname = sprintf("%s/FD_MATLAB_%d_dt_%.2e_Nx_%d_n_relax_%d_",outdir,max_it,dt, GridSize, n_relax);
-% tStart_FD = tic;
-% [t_out, phi_t, delta_mass_t, E_t] = CahnHilliard_FD_SMG(phi0,...
-%                                     t_iter = max_it,...
-%                                     dt = dt,...
-%                                     m = m,...
-%                                     boundary = boundary,...
-%                                     printphi=print_phi,...
-%                                     pathname=pathname,...
-%                                     dt_out = dt_out);
-% elapsedTime = toc(tStart_FD);
-
-% fid = fopen('../Job_specs.csv', 'a+');
-% v = [string(datetime) "FD_spinodal_decomp_smoothed_print" "MATLAB" "FD" GridSize epsilon dt 'NaN' max_it 'NaN' elapsedTime, pathname];
-% fprintf(fid, '%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n', v);
-% fclose(fid);
-
-% % writematrix(phi_t(:,:,end),sprintf('%sfinal_phi.csv', pathname));
-% writematrix(delta_mass_t,sprintf('%smass.csv', pathname));
-% writematrix(E_t,sprintf('%senergy.csv', pathname));
-% t_out = 0:10*dt:max_it*dt;
-% filename = strcat(pathname, "movie_long");
-% phi_file = strcat(pathname, "phi.csv");
-% fprintf("Creating movie\n");
-% if print_phi
-%     ch_movie_from_file(strcat(pathname,"phi.csv"), t_out, ny,filename = filename)
-% else
-%     ch_movie(phi_t,t_out, filename = filename);
-% end
--- 4_vcell_to_ch/normalize_CPC_modified.py
+++ 4_vcell_to_ch/normalize_CPC_modified.py
@@ -1,120 +1,135 @@
 import pandas as pd
 import os
 import numpy as np
-in_dir = "/Users/catalinaalvarez/Documents/cpc_data_2023/"
-
+# in_dir = "/Users/catalinaalvarez/Documents/cpc_data_2023/"
 # folder_name = 'SimID_261879028_0__exported'
 # model_name = "10_16_23_CPC_relaxed_RefModel_128x64"
 # simulation_name = "10_16_23_relaxed_RefModel_Mps1_phos_Plk1a_20Pac_transactiv"
 # outdir = '/Users/smgroves/Documents/GitHub/Cahn_Hilliard_Model/data/'
 # timepoint = 500
 
+
 def pad_with(vector, pad_width, iaxis, kwargs):
     pad_value = kwargs.get('padder', 0)
     vector[:pad_width[0]] = pad_value
-    if pad_width[1] != 0:                      # <-- the only change (0 indicates no padding)
+    # <-- the only change (0 indicates no padding)
+    if pad_width[1] != 0:
         vector[-pad_width[1]:] = pad_value
+
 
 def prolong(uc, nxc, nyc):
     uf = np.zeros((2 * nxc, 2 * nyc))
     for i in range(nxc):
         for j in range(nyc):
-            uf[2 * i][2 * j] = uf[2 * i + 1][2 * j] = uf[2 * i][2 * j + 1] = uf[2 * i + 1][2 * j + 1] = uc[i][j]
+            uf[2 * i][2 * j] = uf[2 * i + 1][2 * j] = uf[2 *
+                                                         i][2 * j + 1] = uf[2 * i + 1][2 * j + 1] = uc[i][j]
     return uf
 
-def rescale_vcell_output(folder_name, in_dir, model_name = "", simulation_name = "", timepoint = 200,
-                         timestep = 10, min_mix = 4, rescaling_factor = 8.4):
+
+def rescale_vcell_output(folder_name, in_dir, model_name="", simulation_name="", timepoint=200,
+                         timestep=10, min_mix=4, rescaling_factor=8.4):
 
     data = {}
     timeslice_id = "00" + str(int(timepoint/timestep))
     # CPC_species = ["CPCi",'CPCa','pH2A_Sgo1_CPCa', 'pH2A_Sgo1_CPCi', 'pH2A_Sgo1_pH3_CPCa', 'pH2A_Sgo1_pH3_CPCi','pH3_CPCa', 'pH3_CPCi']
-    for file in os.listdir(os.path.join(in_dir,folder_name)):
+    for file in os.listdir(os.path.join(in_dir, folder_name)):
         if "CPC" in file:
             if timeslice_id in file:
                 name = file.split("0_")[-1].split(f"_{timeslice_id}.")[0]
-                data[name] = pd.read_csv(os.path.join(in_dir,folder_name,file), sep=",",
-                                         skiprows = 10, header = None)
+                data[name] = pd.read_csv(os.path.join(in_dir, folder_name, file), sep=",",
+                                         skiprows=10, header=None)
 
-    sum_data = pd.DataFrame(0, columns = data["CPCi"].columns, index = data["CPCi"].index)
+    sum_data = pd.DataFrame(
+        0, columns=data["CPCi"].columns, index=data["CPCi"].index)
     for key in data.keys():
         sum_data = sum_data.add(data[key])
 
     sum_data_array = np.array(sum_data)
     sum_data_array = (sum_data_array - min_mix)/(rescaling_factor - min_mix)
 
-
-    #pad the sides of the array with zeros so it is square
+    # pad the sides of the array with zeros so it is square
     width = sum_data_array.shape[0]-sum_data_array.shape[1]
-    sum_data_array = (np.pad(sum_data_array, ((0, 0), (int(width/2), int(width/2))), pad_with, padder=0))
+    sum_data_array = (np.pad(
+        sum_data_array, ((0, 0), (int(width/2), int(width/2))), pad_with, padder=0))
 
-    nrows= sum_data_array.shape[0]
-    ncols= sum_data_array.shape[1]
+    nrows = sum_data_array.shape[0]
+    ncols = sum_data_array.shape[1]
     sum_data_array.max()
 
-    np.savetxt(os.path.join(in_dir,folder_name,f"{model_name}_{simulation_name}_{timepoint}_{nrows}x{ncols}.csv"), sum_data_array, delimiter=",")
+    np.savetxt(os.path.join(in_dir, folder_name,
+               f"{model_name}_{simulation_name}_{timepoint}_{nrows}x{ncols}.csv"), sum_data_array, delimiter=",")
 
     arr_2fold = prolong(sum_data_array, nrows, ncols)
-    np.savetxt(os.path.join(in_dir,folder_name,f"{model_name}_{simulation_name}_{timepoint}_{arr_2fold.shape[0]}x{arr_2fold.shape[1]}.csv"), arr_2fold, delimiter=",")
+    np.savetxt(os.path.join(in_dir, folder_name,
+               f"{model_name}_{simulation_name}_{timepoint}_{arr_2fold.shape[0]}x{arr_2fold.shape[1]}.csv"), arr_2fold, delimiter=",")
 
     arr_4fold = prolong(arr_2fold, arr_2fold.shape[0], arr_2fold.shape[1])
-    np.savetxt(os.path.join(in_dir,folder_name,f"{model_name}_{simulation_name}_{timepoint}_{arr_4fold.shape[0]}x{arr_4fold.shape[1]}.csv"), arr_4fold, delimiter=",")
+    np.savetxt(os.path.join(in_dir, folder_name,
+               f"{model_name}_{simulation_name}_{timepoint}_{arr_4fold.shape[0]}x{arr_4fold.shape[1]}.csv"), arr_4fold, delimiter=",")
 
-def rescale_vcell_output_neg1_pos1(folder_name, in_dir, outdir, model_name = "", simulation_name = "", timepoint = 200,
-                         timestep = 10, min_mix = 2, rescaling_factor = 5, suffix = ""):
+
+def rescale_vcell_output_neg1_pos1(folder_name, in_dir, outdir, model_name="", simulation_name="", timepoint=200,
+                                   timestep=10, min_mix=2, rescaling_factor=5, suffix=""):
 
     data = {}
     if timepoint == 0:
         timeslice_id = "0000"
-    elif timepoint < 100:
-        timeslice_id= "000" + str(int(timepoint/timestep))
+    elif (timepoint < 100) and (timestep == 10):
+        timeslice_id = "000" + str(int(timepoint/timestep))
+    elif (timepoint < 100) and (timestep == 1):
+        timeslice_id = "00" + str(int(timepoint/timestep))
     else:
         timeslice_id = "00" + str(int(timepoint/timestep))
 
     print(timeslice_id)
     # CPC_species = ["CPCi",'CPCa','pH2A_Sgo1_CPCa', 'pH2A_Sgo1_CPCi', 'pH2A_Sgo1_pH3_CPCa', 'pH2A_Sgo1_pH3_CPCi','pH3_CPCa', 'pH3_CPCi']
-    for file in os.listdir(os.path.join(in_dir,folder_name)):
+    for file in os.listdir(os.path.join(in_dir, folder_name)):
         if "CPC" in file:
             if timeslice_id in file:
                 name = file.split("0_")[-1].split(f"_{timeslice_id}.csv")[0]
                 print(name)
-                data[name] = pd.read_csv(os.path.join(in_dir,folder_name,file), sep=",",
-                                         skiprows = 10, header = None)
+                data[name] = pd.read_csv(os.path.join(in_dir, folder_name, file), sep=",",
+                                         skiprows=10, header=None)
 
-    sum_data = pd.DataFrame(0, columns = data["CPCi"].columns, index = data["CPCi"].index)
+    sum_data = pd.DataFrame(
+        0, columns=data["CPCi"].columns, index=data["CPCi"].index)
     for key in data.keys():
         sum_data = sum_data.add(data[key])
 
     sum_data_array = np.array(sum_data)
-    #sum_data_array = sum_data_array/rescaling_factor
+    # sum_data_array = sum_data_array/rescaling_factor
     print(sum_data_array.max())
     print(sum_data_array.min())
     sum_data_array = (sum_data_array - min_mix)/(rescaling_factor - min_mix)
     print(sum_data_array.max())
     print(sum_data_array.min())
-    
+
     for r_idx, row in enumerate(sum_data_array):
         for c_idx, value in enumerate(row):
             if value == (- min_mix)/(rescaling_factor - min_mix):
                 sum_data_array[r_idx][c_idx] = 0
-    
+
     print(sum_data_array.max())
     print(sum_data_array.min())
 
-    #pad the sides of the array with zeros so it is square
+    # pad the sides of the array with zeros so it is square
     width = sum_data_array.shape[0]-sum_data_array.shape[1]
 
-    sum_data_array = (np.pad(sum_data_array, ((0, 0), (int(width/2), int(width/2))), pad_with, padder=0))
-    nrows= sum_data_array.shape[0]
-    ncols= sum_data_array.shape[1]
+    sum_data_array = (np.pad(
+        sum_data_array, ((0, 0), (int(width/2), int(width/2))), pad_with, padder=0))
+    nrows = sum_data_array.shape[0]
+    ncols = sum_data_array.shape[1]
     sum_data_array = (2*sum_data_array) - 1
     print(sum_data_array.max())
     print(sum_data_array.min())
 
-    np.savetxt(os.path.join(outdir,f"{model_name}_{simulation_name}_{timepoint}_{nrows}x{ncols}_{suffix}.csv"), sum_data_array, delimiter=",")
-    
+    np.savetxt(os.path.join(
+        outdir, f"{model_name}_{simulation_name}_{timepoint}_{nrows}x{ncols}_{suffix}.csv"), sum_data_array, delimiter=",")
+
     arr_2fold = prolong(sum_data_array, nrows, ncols)
     print(arr_2fold.shape)
-    np.savetxt(os.path.join(outdir,f"{model_name}_{simulation_name}_{timepoint}_{arr_2fold.shape[0]}x{arr_2fold.shape[1]}_{suffix}.csv"), arr_2fold, delimiter=",")
+    np.savetxt(os.path.join(
+        outdir, f"{model_name}_{simulation_name}_{timepoint}_{arr_2fold.shape[0]}x{arr_2fold.shape[1]}_{suffix}.csv"), arr_2fold, delimiter=",")
 
     #
     # arr_2fold = prolong(sum_data_array, nrows, ncols)
@@ -124,84 +139,29 @@ def rescale_vcell_output_neg1_pos1(folder_name, in_dir, outdir, model_name = "",
     # np.savetxt(os.path.join(outdir,folder_name,f"{model_name}_{simulation_name}_{timepoint}_{arr_4fold.shape[0]}x{arr_4fold.shape[1]}{suffix}.csv"), arr_4fold, delimiter=",")
 
 # rescale_vcell_output_neg1_pos1(folder_name, in_dir, outdir, model_name = model_name, simulation_name = simulation_name, timepoint = 100,
-                        #  timestep = 10, rescaling_factor = 10, suffix = "100s_10max_")
-
-# folder_name ="SimID_270418727_0__exported"
-# model_name = "03_25_24_CPC_relaxed_RefModel_128x64"
-# simulation_name = "03_25_24_relaxed_RefModel"
-# outdir = '/Users/smgroves/Documents/GitHub/Cahn_Hilliard_Model/data/'
-# rescale_vcell_output_neg1_pos1(folder_name, in_dir, outdir, model_name = model_name, simulation_name = simulation_name, timepoint = 200,
-#                          timestep = 10, rescaling_factor = 5, suffix = "200s_5max")
-# # rescale_vcell_output_neg1_pos1(folder_name, in_dir, outdir, model_name = model_name, simulation_name = simulation_name, timepoint = 200,
-# #                          timestep = 10, rescaling_factor = 10, suffix = "200s_10max")
-# # rescale_vcell_output_neg1_pos1(folder_name, in_dir, outdir, model_name = model_name, simulation_name = simulation_name, timepoint = 200,
-# #                          timestep = 10, rescaling_factor = 7, suffix = "200s_7max")
-
-# folder_name = "SimID_270418727_0__exported"
-# model_name="03_25_24_CPC_relaxed_RefModel_128x64"
-# simulation_name ="03_25_24_relaxed_RefModel"
-# outdir = '/Users/smgroves/Documents/GitHub/Cahn_Hilliard_Model/data/'
-# for timepoint in [100,200,300,400]:
-#     rescale_vcell_output_neg1_pos1(folder_name, in_dir, outdir, model_name = model_name, simulation_name = simulation_name, timepoint = timepoint,
-#                          timestep = 10, rescaling_factor = 8.4, suffix = f"{timepoint}s_8.4max")
-
-# folder_name = "SimID_270510934_0__exported"
-# model_name="03_25_24_CPC_tensed_RefModel_128x64"
-# simulation_name ="04_02_24_tensed_RefModel"
-# outdir = '/Users/smgroves/Documents/GitHub/Cahn_Hilliard_Model/data/'
-# for timepoint in [100,200,300,400]:
-#     rescale_vcell_output_neg1_pos1(folder_name, in_dir, outdir, model_name = model_name, simulation_name = simulation_name, timepoint = timepoint,
-#                          timestep = 10, rescaling_factor = 8.4, suffix = f"{timepoint}s_8.4max")
-
-# folder_name = "SimID_262253748_0__exported"
-# model_name="10_24_23_CPC_tensed_RefModel_128x64_post_transition"
-# simulation_name ="10_25_23_400s_post_transition_base_20Pac"
-# outdir = '/Users/smgroves/Documents/GitHub/Cahn_Hilliard_Model/data/'
-# for timepoint in [00, 100,200,300,400]:
-#     print(timepoint)
-#     rescale_vcell_output_neg1_pos1(folder_name, in_dir, outdir, model_name = model_name, simulation_name = simulation_name, timepoint = timepoint,
-#                          timestep = 10, rescaling_factor = 8.4, suffix = f"{timepoint}s_8.4max")
-
-# folder_name = "SimID_261879028_0__exported"
-# model_name="10_16_23_CPC_relaxed_RefModel_128x64"
-# simulation_name ="10_16_23_relaxed_RefModel_Mps1_phos_Plk1a_20Pac_transactiv"
-# outdir = '/Users/smgroves/Documents/GitHub/Cahn_Hilliard_Model/data/'
-# for timepoint in [00, 100,200,300,400]:
-#     print(timepoint)
-#     rescale_vcell_output_neg1_pos1(folder_name, in_dir, outdir, model_name = model_name, simulation_name = simulation_name, timepoint = timepoint,
-#                          timestep = 10, rescaling_factor = 8.4, suffix = f"{timepoint}s_8.4max")
-    
-
-# folder_name = "SimID_272623071_0__exported"
-# model_name="10_24_23_CPC_tensed_RefModel_128x64_post_transition"
-# simulation_name ="07_14_24_500s_post_transition_base_20Pac"
-# outdir = '/Users/smgroves/Documents/GitHub/Cahn_Hilliard_Model/data/'
-# for timepoint in [00, 10, 20, 50]:
-#     print(timepoint)
-#     rescale_vcell_output_neg1_pos1(folder_name, in_dir, outdir, model_name = model_name, simulation_name = simulation_name, timepoint = timepoint,
-#                          timestep = 10, rescaling_factor = 8.4, suffix = f"{timepoint}s_8.4max")
-    
-
-# folder_name = "SimID_261879028_0__exported"
-# model_name="10_16_23_CPC_relaxed_RefModel_128x64"
-# simulation_name ="10_16_23_relaxed_RefModel_Mps1_phos_Plk1a_20Pac_transactiv"
-# outdir = '/Users/smgroves/Documents/GitHub/Cahn_Hilliard_Model/data/'
-# for timepoint in [50, 70]:
-#     print(timepoint)
-#     rescale_vcell_output_neg1_pos1(folder_name, in_dir, outdir, model_name = model_name, simulation_name = simulation_name, timepoint = timepoint,
-#                          timestep = 10, rescaling_factor = 8.4, suffix = f"{timepoint}s_8.4max")
+    #  timestep = 10, rescaling_factor = 10, suffix = "100s_10max_")
 
 
-folder_name = "SimID_296831234_2__exported"
-model_name= "09_16_25 CPC_metacentric_relaxed_model "
-simulation_name = "09_30_25_metacentric_relaxed_model_HeLa_CPC_Sgo1low_pH2Ainh_1"
-outdir = '/Users/catalinaalvarez/Documents/cpc_ch_data_2025/'
-rescale_vcell_output_neg1_pos1(folder_name, in_dir, outdir, model_name = model_name, simulation_name = simulation_name, timepoint = 200,
-                         timestep = 10, min_mix = 2, rescaling_factor = 5, suffix = "0s_8.4max")
-    
-# folder_name = "SimID_261879026_0__exported"
-# model_name="10_16_23_CPC_relaxed_RefModel_128x64"
-# simulation_name ="10_16_23_relaxed_RefModel_Mps1_phos_Plk1a_transactiv"
-# outdir = '/Users/smgroves/Documents/GitHub/Cahn_Hilliard_Model/data/'
-# rescale_vcell_output_neg1_pos1(folder_name, in_dir, outdir, model_name = model_name, simulation_name = simulation_name, timepoint = 70,
-#                          timestep = 10, rescaling_factor = 8.4, suffix = f"70s_8.4max")
+# in_dir = "/Users/smgroves/Documents/GitHub/VCell_Analysis/vcell_out/10_30_25_CPC_metacentric_transition_model"
+# folder_name = "SimID_298553153_0__exported"
+# model_name = "10_30_25 CPC_metacentric_transition_model"
+# simulation_name = "11_06_2025_transition_model_2ummin_KTmovement_NDC80avail_0.1_fixed_delT_18s"
+# outdir = "/Users/smgroves/Documents/GitHub/VCell_Analysis/4_vcell_to_ch/IC"
+# rescale_vcell_output_neg1_pos1(folder_name, in_dir, outdir, model_name=model_name, simulation_name=simulation_name, timepoint=18,
+#                                timestep=1, min_mix=2, rescaling_factor=5, suffix="_5max")
+
+in_dir = "/Users/smgroves/Documents/GitHub/VCell_Analysis/vcell_out/09_16_25_CPC_metacentric_tensed_model_v2"
+folder_name = "SimID_296945950_0__exported"
+model_name = "09_16_25_CPC_metacentric_tensed_model_v2"
+simulation_name = "09_16_25_metacentric_tensed_model"
+outdir = "/Users/smgroves/Documents/GitHub/VCell_Analysis/4_vcell_to_ch/IC"
+rescale_vcell_output_neg1_pos1(folder_name, in_dir, outdir, model_name=model_name, simulation_name=simulation_name, timepoint=120,
+                               timestep=10, min_mix=2, rescaling_factor=5, suffix="_5max")
+
+in_dir = "/Users/smgroves/Documents/GitHub/VCell_Analysis/vcell_out/_09_16_25_CPC_metacentric_relaxed_model"
+folder_name = "SimID_296945372_0__exported"
+model_name = "_09_16_25_CPC_metacentric_relaxed_model"
+simulation_name = "09_16_25_metacentric_relaxed_model"
+outdir = "/Users/smgroves/Documents/GitHub/VCell_Analysis/4_vcell_to_ch/IC"
+rescale_vcell_output_neg1_pos1(folder_name, in_dir, outdir, model_name=model_name, simulation_name=simulation_name, timepoint=120,
+                               timestep=10, min_mix=2, rescaling_factor=5, suffix="_5max")
